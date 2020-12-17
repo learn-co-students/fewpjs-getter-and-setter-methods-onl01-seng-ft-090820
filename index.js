6d4a4b79a81562e294f90f697c74174d999518fd
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-12-17 16:55:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
-// Add your Circle class here
-const PI = Math.PI
+// // Add your Circle class here
+// const PI = Math.PI
+// class Circle {
+//     constructor(radius) {
+//         this.radius = radius;
+//     }
+
+//     get diameter() {
+//         return 2 * this.radius
+//     }
+
+//     get circumference() {
+//         return 2 * PI * this.radius
+//     }
+//     get area() {
+//         return PI * this.radius * this.radius
+//     }
+
+//     set diameter(radius) {
+//         if (radius > 1) {
+//             this._radius = radius
+//                 //this._radius = radius;
+//         }
+//     }
+
+//     set circumference(radius) {
+//         this.radius = circumference / (pi * 2)
+//     }
+
+//     set area(radius) {
+//         if (radius > 1) {
+//             this._radius = radius
+//         }
+//     }
+// }
+
+const pi = Math.PI
+
 class Circle {
     constructor(radius) {
         this.radius = radius;
     }
 
     get diameter() {
-        return 2 * this.radius
+        radius * 2
+    }
+
+    set diameter(diameter) {
+        diameter / 2
     }
 
     get circumference() {
-        return 2 * PI * this.radius
+        this.radius * this.radius * pi
     }
+
+    set circumference(circumference) {
+        radius = circumference / (pi * 2)
+    }
+
     get area() {
-        return PI * this.radius * this.radius
+        pi * (radius * radius)
     }
 
-    set diameter(radius) {
-        if (radius > 1) {
-            this._radius = radius
-                //this._radius = radius;
-        }
-    }
-
-    set circumference(radius) {
-        this.radius = circumference / (pi * 2)
-    }
-
-    set area(radius) {
-        if (radius > 1) {
-            this._radius = radius
-        }
+    set area(area) {
+        radius = sqrt(area / pi)
     }
 }
